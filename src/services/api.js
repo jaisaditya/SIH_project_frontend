698491f6035d6e6e1abc7d92863263e03faa7523
jaisaditya <aj222004a@gmail.com>
2025-09-25 02:00:39 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ import axios from "axios";
 // });
 
 const API = axios.create({
-  baseURL: "https://sih-project-backend-7l8d.onrender.com//api",
+  baseURL: "https://sih-project-backend-7l8d.onrender.com/api",
 });
 
 
