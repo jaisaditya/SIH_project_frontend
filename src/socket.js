ef70b30afd6d885c4aa00c30b1ae12880d974da4
jaisaditya <aj222004a@gmail.com>
2025-09-25 01:54:45 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 // ⚠️ Replace with your backend URL (http://localhost:5000 in dev)
 // const SOCKET_URL = "http://localhost:5000";
-const SOCKET_URL = "https://sih-project-backend-7l8d.onrender.com/";
+const SOCKET_URL = "https://sih-project-backend-7l8d.onrender.com";
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket"],
