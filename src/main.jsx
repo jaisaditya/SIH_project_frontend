799ab5595cc5da78f93fdccb4b53c1cece32a1f7
jaisaditya <aj222004a@gmail.com>
2025-09-27 22:28:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,6 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      // new
-      // main.jsx (temporary debug lines)
-console.log("[Google OAuth] clientId:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
-console.log("[Google OAuth] clientId length:", (import.meta.env.VITE_GOOGLE_CLIENT_ID || "").length);
-
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
